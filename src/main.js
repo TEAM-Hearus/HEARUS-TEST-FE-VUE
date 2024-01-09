27507ffbd287bad0a2460e97d216ec0d4337ff4b
judemin <judemin2087@naver.com>
2024-01-09 13:14:45 +0900

    TEST : Test AWS ECR push image

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue';
 import io from 'socket.io-client';
 
 console.log(process.env.VUE_APP_BACKEND_HOST);
-console.log("Config Env Var : " + config.VUE_CONFIG_APP_API);
 const socket = io(process.env.VUE_APP_BACKEND_HOST, {
     // Auto Reconnect
     reconnection: true,
