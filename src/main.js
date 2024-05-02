9e00cafa3c3303909f2c54d9b230d1421cd2fd87
unknown <judemin2087@naver.com>
2024-05-02 13:23:21 +0900

    UPDATE : UPDATE socket.io configureation

--- src/main.js
+++ src/main.js
@@ -3,16 +3,25 @@ import App from './App.vue';
 import io from 'socket.io-client';
 
 console.log("process.env.VUE_APP_BACKEND_HOST : ", process.env.VUE_APP_BACKEND_HOST)
+console.log("process.env.VUE_APP_SOCKETIO_HOST : ", process.env.VUE_APP_SOCKETIO_HOST)
 console.log("window.VUE_APP_K8S : ", window.VUE_APP_K8S);
-let socketUrl = process.env.NODE_ENV === 'development' ? process.env.VUE_APP_BACKEND_HOST : window.VUE_APP_K8S;
+
+let socketUrl = process.env.NODE_ENV === 'development' 
+  ? '${process.env.VUE_APP_SOCKETIO_HOST}' 
+  : '${window.VUE_APP_K8S}';
+  console.log("socketURL : ", socketUrl)
 
 const socket = io(socketUrl, {
+    path: '/socketio',
     // Auto Reconnect
     reconnection: true,
     reconnectionDelay: 5000,
     reconnectionAttempts: Infinity,
     transports: ["websocket"],
     withCredentials: true,
+    extraHeaders: {
+      'Access-Control-Allow-Origin': '*'
+    }
 });
 
 
